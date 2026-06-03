2be358d95323000ab694a273409b9cf9d30521b3
Viktoryyyyy <145966014+Viktoryyyyy@users.noreply.github.com>
2026-06-03 13:55:27 +0300

    Add registry contract exports

---
+++ src/moex_core/contracts/__init__.py
@@ -0,0 +1,23 @@
+from .registry_contracts import (
+    EXPECTED_REGISTRY_CONFIG_PATHS,
+    EXPECTED_REGISTRY_CONTRACT_PATHS,
+    REGISTRY_KINDS,
+    RegistryContractError,
+    RegistryEntry,
+    RegistryPackage,
+    validate_registry_entry_values,
+    validate_registry_package,
+    validate_registry_package_values,
+)
+
+__all__ = (
+    "EXPECTED_REGISTRY_CONFIG_PATHS",
+    "EXPECTED_REGISTRY_CONTRACT_PATHS",
+    "REGISTRY_KINDS",
+    "RegistryContractError",
+    "RegistryEntry",
+    "RegistryPackage",
+    "validate_registry_entry_values",
+    "validate_registry_package",
+    "validate_registry_package_values",
+)
