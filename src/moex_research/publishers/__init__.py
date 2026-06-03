01945fb4157e05db61a4dfac4971aa088902b023
Viktoryyyyy <145966014+Viktoryyyyy@users.noreply.github.com>
2026-06-03 17:11:50 +0300

    Keep research publishers package init stable

--- src/moex_research/publishers/__init__.py
+++ src/moex_research/publishers/__init__.py
@@ -1,8 +1,3 @@
 from .artifact_manifest import build_artifact_manifest, validate_publishable_artifact_manifest
-from .review_artifact import build_pm_review_artifact
 
-__all__ = [
-    "build_artifact_manifest",
-    "build_pm_review_artifact",
-    "validate_publishable_artifact_manifest",
-]
+__all__ = ["build_artifact_manifest", "validate_publishable_artifact_manifest"]
